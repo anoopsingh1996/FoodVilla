de7b71d26fe9bf670dacfea67ae50dda51bbca9e
anoopsingh1996 <anoopraghuvansi@gmail.com>
2023-04-17 17:34:45 +0530

    Chapter 07

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+const About = () =>{
+    return (
+        <div>
+            <h1>About Us Page</h1>
+            <p>
+                {" "}
+                This is me *****
+            </p>
+        </div>
+        
+    );
+}
+
+export default About;
